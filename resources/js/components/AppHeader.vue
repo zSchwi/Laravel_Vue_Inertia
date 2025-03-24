27343531bcbb14fd908233ca7638191442945062
zSchwi <rafa.fmorais2001@gmail.com>
2025-03-24 16:50:52 -0300

    fix: alteracao na navbar e estilizacoes globais

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -127,7 +127,7 @@ const rightNavItems: NavItem[] = [
                                 </Link>
                                 <div
                                     v-if="isCurrentRoute(item.href)"
-                                    class="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"
+                                    class="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-blue-600 dark:bg-white"
                                 ></div>
                             </NavigationMenuItem>
                         </NavigationMenuList>
